5427b8a2232ae132522480af4448af44e7446b04
Dysthesis <antheoraviel@protonmail.com>
2025-06-14 14:28:31 +1000

    Better document the search module

--- src/search.rs
+++ src/search.rs
@@ -26,8 +26,6 @@ use serde::Serialize;
 ///
 /// k_1 and b are optimisation parameters, with the usual values being k_1 in [1.2, 2.0] and b =
 /// 0.75.
-///
-/// Because I don't know what's going on here, I'll just randomly choose k_1 as 1.6.
 #[derive(Serialize, Debug)]
 pub struct Corpus {
     docs: Vec<String>,
@@ -36,22 +34,31 @@ pub struct Corpus {
 }
 
 impl Corpus {
+    /// Because I don't know what's going on here, I'll just randomly choose k_1 as 1.6.
     pub const K1: f32 = 1.6;
     pub const B: f32 = 0.75;
 
+    /// Initilise a new corpus and calculate its statistics
+    // NOTE: Figure out if we can guarantee that this document is definitely found in the corpus
     pub fn new(docs: Vec<String>) -> Self {
+        // Find the average length of a document in the corpus
         let avgdl = docs
             .iter()
             .map(|doc| doc.split_whitespace().count() as f32)
             .sum::<f32>()
             / docs.len() as f32;
+
+        // Calculate the document frequency
         let df = docs
             .par_iter()
+            // Normalise the text to make it case-insensitive, and flatten it into a set of all
+            // tokens
             .flat_map(|doc| {
                 doc.split_whitespace()
                     .map(str::to_ascii_lowercase)
                     .collect::<HashSet<_>>()
             })
+            // Calculate the occurrence of each token
             .fold(HashMap::new, |mut acc: HashMap<String, f32>, curr| {
                 *acc.entry(curr).or_default() += 1f32;
                 acc
@@ -63,6 +70,7 @@ impl Corpus {
                 a
             });
 
+        // Calculate the inverse document frequency of each token from the document frequency
         let idf = df
             .into_iter()
             .map(|(term, num_occurrence)| {
@@ -74,17 +82,20 @@ impl Corpus {
         Self { docs, avgdl, idf }
     }
 
-    // TODO: See if there are any possible overflows due to the typecasting
     pub fn score(&self, query: &str, document: &str) -> f32 {
         let dl = document.split_whitespace().count() as f32;
         let norm = Self::K1 * (1f32 - Self::B + Self::B * dl / self.avgdl);
+
+        // Find out how many times each term shows up in the given document
         let tf: HashMap<&str, usize> = document.split_whitespace().fold(
             HashMap::new(),
-            |mut m: HashMap<&str, usize>, term| {
-                *m.entry(term).or_default() += 1;
-                m
+            |mut frequencies: HashMap<&str, usize>, term| {
+                *frequencies.entry(term).or_default() += 1;
+                frequencies
             },
         );
+
+        // Calculate the BM25 score of each term in the query
         query
             .split_whitespace()
             .map(|term| {
