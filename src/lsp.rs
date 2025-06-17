212d6f3f0a6b6aa57a33756958536c0c1a32d1ec
Dysthesis <antheoraviel@protonmail.com>
2025-06-17 16:02:33 +1000

    Maintain a map of open documents

--- src/lsp.rs
+++ src/lsp.rs
@@ -1,11 +1,14 @@
 //! # LSP module
 
+use dashmap::DashMap;
+use ropey::Rope;
 use tower_lsp::{
     Client, LanguageServer, LspService, Server,
     jsonrpc::Result,
     lsp_types::{
-        CompletionParams, CompletionResponse, InitializeParams, InitializeResult,
-        InitializedParams, MessageType,
+        CompletionParams, CompletionResponse, DidChangeTextDocumentParams,
+        DidOpenTextDocumentParams, InitializeParams, InitializeResult, InitializedParams,
+        MessageType, Position, Range, TextDocumentContentChangeEvent, Url,
     },
 };
 use tracing::{info, trace, warn};
@@ -13,6 +16,8 @@ use tracing::{info, trace, warn};
 #[derive(Debug)]
 pub struct Backend {
     client: Client,
+    /// Maps a Url to the document
+    documents: DashMap<Url, Rope>,
 }
 
 #[tower_lsp::async_trait]
@@ -34,6 +39,48 @@ impl LanguageServer for Backend {
     async fn completion(&self, params: CompletionParams) -> Result<Option<CompletionResponse>> {
         todo!()
     }
+
+    async fn did_open(&self, params: DidOpenTextDocumentParams) {
+        let text = params.text_document.text;
+        let rope = Rope::from(text);
+        self.documents.insert(params.text_document.uri, rope);
+    }
+    async fn did_change(&self, params: DidChangeTextDocumentParams) {
+        pub fn position_to_offset(rope: &Rope, position: Position) -> Option<usize> {
+            let (line, col) = (position.line as usize, position.character as usize);
+            if line == rope.len_lines() && col == 0 {
+                return Some(rope.len_chars());
+            }
+            (line < rope.len_lines()).then_some(line).and_then(|line| {
+                let col_offset = rope.line(line).try_byte_to_char(col).ok()?;
+                let offset = rope.try_line_to_char(line).ok()? + col_offset;
+                Some(offset)
+            })
+        }
+
+        let url = params.text_document.uri;
+        if let Some(mut rope) = self.documents.get_mut(&url) {
+            for change in params.content_changes {
+                let TextDocumentContentChangeEvent { range, text, .. } = change;
+                match range {
+                    // incremental change
+                    Some(Range { start, end }) => {
+                        let start = position_to_offset(&rope, start);
+                        let end = position_to_offset(&rope, end);
+                        if let (Some(s), Some(e)) = (start, end) {
+                            rope.remove(s..e);
+                            rope.insert(s, &text);
+                        }
+                    }
+
+                    // full content change
+                    None => {
+                        *rope = Rope::from(text);
+                    }
+                }
+            }
+        }
+    }
 }
 
 impl Backend {
@@ -42,7 +89,10 @@ impl Backend {
         let stdin = tokio::io::stdin();
         let stdout = tokio::io::stdout();
 
-        let (service, socket) = LspService::new(|client| Backend { client });
+        let (service, socket) = LspService::new(|client| Backend {
+            client,
+            documents: DashMap::new(),
+        });
         info!("Initialised LSP backend!");
 
         Server::new(stdin, stdout, socket).serve(service).await;
