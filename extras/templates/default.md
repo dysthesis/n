8abcbbdbaa0a526d47cd2e1735b9f3c91b0c36e6
Dysthesis <antheoraviel@protonmail.com>
2025-06-18 22:57:17 +1000

    Change nn script

--- extras/templates/default.md
+++ extras/templates/default.md
@@ -1,5 +1,5 @@
 ---
-title: { { title } }
-date: { { date } }
-id: { { id } }
+title: {{title}}
+created: {{created}}
+id: {{id}}
 ---
--- nix/packages/nn.nix
+++ nix/packages/nn.nix
@@ -1,15 +1,55 @@
 {
   lib,
-  writeShellScriptBin,
+  writeShellApplication,
   n,
   vim,
+  util-linux,
+  coreutils,
+  shellcheck,
+  template ? "${../../extras/templates/default.md}",
+  name ? "nn",
 }:
-let
-  inherit (lib) getExe;
-in
-writeShellScriptBin "nn" ''
-  NOTES_DIR="''${NOTES_DIR:-$HOME/Documents/Notes/Contents}"
-  EDITOR="''${EDITOR:-${getExe vim}}"
-  ID="$(date +"%Y%m%d%H%M")"
-  $EDITOR "$(${getExe n} -d "$NOTES_DIR" new -t "${../../extras/templates/default.md}" -v "title:$@,id:$ID,date:$(date)" "$ID - $@")"
-''
+writeShellApplication {
+  inherit name;
+  runtimeInputs = [
+    n
+    vim
+    util-linux
+    coreutils
+  ];
+
+  checkPhase = ''
+    runHook preCheck
+    ${lib.getExe shellcheck} $target
+    runHook postCheck
+  '';
+
+  text = ''
+    # Exit on error
+    set -e
+
+    NOTES_DIR="''${NOTES_DIR:-''${XDG_DOCUMENTS_DIR:-$HOME/Documents}/Notes/Contents}"
+    EDITOR="''${EDITOR:-${lib.getExe vim}}"
+
+    ID="$(uuidgen)"
+    DATE="$(date +"%Y-%m-%d %H\:%M")"
+
+    # CORRECTED LINE: Use "$*" to join all arguments into a single string.
+    TITLE="$*"
+
+    # Handle the case where no title is given
+    if [ -z "$TITLE" ]; then
+      echo "Error: Please provide a title for the note." >&2
+      exit 1
+    fi
+
+    FILE="$(${lib.getExe n} -d "$NOTES_DIR" new -t "${template}" -v "title:$TITLE,id:$ID,created:$DATE" "$TITLE")"
+
+    if [ -z "$FILE" ]; then
+      echo "Error: Failed to create new note. 'n' command returned an empty path." >&2
+      exit 1
+    fi
+
+    exec "$EDITOR" "$FILE"
+  '';
+}
